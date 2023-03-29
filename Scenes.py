b3504c1bec9d8d79468a7c08aa449e7582a6b8f0
Roan <rpklazema@gmail.com>
2023-03-29 21:04:28 +0200

    grass random

--- Scenes.py
+++ Scenes.py
@@ -9,6 +9,19 @@ grasspng3 = pygame.transform.scale(pygame.image.load("Textures/Grass texture3.pn
 class Scene:
     def __init__(self) -> None:
         self.scene_objects = []
+
+        self.grassmap = []
+        for x in range(0,20):
+            self.grassmap.append([])
+            for y in range (0, 20):
+                randompng = random.randint(0,2)
+                if randompng == 0:
+                    self.grasspng = grasspng1
+                if randompng == 1:
+                    self.grasspng = grasspng2
+                if randompng == 2:
+                    self.grasspng = grasspng3
+                self.grassmap[x].append(self.grasspng)
     
     def render(self, screen: pygame.Surface):
         for i in range(0,20):
@@ -17,4 +30,4 @@ class Scene:
             pygame.draw.aaline(screen, (0, 0, 0), (0, i*100), (1280, i*100))
         for x in range(0,20):
             for y in range(0,20):
-                screen.blit(grasspng1, (x*96, y*96))
+                screen.blit(self.grassmap[x] [y], (x*96, y*96))
